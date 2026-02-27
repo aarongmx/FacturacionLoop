070fc0789bb51bc79df0f0949a84bf20f1b93340
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2026-02-26 23:17:12 -0600

    wip: se instala rector

--- rector.php
+++ rector.php
@@ -3,24 +3,32 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Exception\Configuration\InvalidConfigurationException;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Set\LaravelSetProvider;
 
-return RectorConfig::configure()
-    ->withPaths([
-        __DIR__.'/app',
-        __DIR__.'/bootstrap/app.php',
-        __DIR__.'/database',
-        __DIR__.'/public',
-    ])
-    ->withSkip([
-        AddOverrideAttributeToOverriddenMethodsRector::class,
-    ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        codingStyle: true,
-    )
-    ->withPhpSets();
+try {
+    return RectorConfig::configure()
+        ->withPaths([
+            __DIR__ . '/app',
+            __DIR__ . '/bootstrap/app.php',
+            __DIR__ . '/database',
+            __DIR__ . '/public',
+        ])
+        ->withSkip([
+            AddOverrideAttributeToOverriddenMethodsRector::class,
+        ])
+        ->withSetProviders(LaravelSetProvider::class)
+        ->withComposerBased(laravel: true)
+        ->withPreparedSets(
+            deadCode: true,
+            codeQuality: true,
+            codingStyle: true,
+            typeDeclarations: true,
+            privatization: true,
+            earlyReturn: true,
+        )
+        ->withPhpSets();
+} catch (InvalidConfigurationException $e) {
+    logger($e->getMessage());
+}
