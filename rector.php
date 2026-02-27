3b4ac42fbdb2d0775852c30899ba8a000c4103a0
Aarón Gómez Méndez <aaron.gomez.mendez@gmail.com>
2026-02-26 23:40:25 -0600

    wip

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Config\RectorConfig;
 use Rector\Exception\Configuration\InvalidConfigurationException;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use RectorLaravel\Set\LaravelSetList;
 use RectorLaravel\Set\LaravelSetProvider;
 
 try {
@@ -19,6 +20,17 @@ try {
             AddOverrideAttributeToOverriddenMethodsRector::class,
         ])
         ->withSetProviders(LaravelSetProvider::class)
+        ->withSets([
+            LaravelSetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
+            LaravelSetList::LARAVEL_COLLECTION,
+            LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
+            LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
+            LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+            LaravelSetList::LARAVEL_FACTORIES,
+            LaravelSetList::LARAVEL_CODE_QUALITY,
+            LaravelSetList::LARAVEL_TESTING,
+            LaravelSetList::LARAVEL_TYPE_DECLARATIONS,
+        ])
         ->withComposerBased(laravel: true)
         ->withPreparedSets(
             deadCode: true,
